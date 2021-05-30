ef2e081711689fea7d0d27302175388868b5d26f
Bryan <bryantello2010@hotmail.com>
2021-05-29 22:01:34 -0600

    Agregada configuración básica de la base de datos

---
+++ gestionDeProductos/db/db.sql
@@ -0,0 +1,16 @@
+-- ------- Creating database ------
+CREATE DATABASE IF NOT EXISTS products;
+
+-- Using db
+USE products;
+
+-- Categories table
+CREATE TABLE IF NOT EXISTS categories(
+
+) ENGINE=InnoDb;
+
+
+-- product table
+CREATE TABLE IF NOT EXISTS products(
+
+)ENGINE=InnoDb;
