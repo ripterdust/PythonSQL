ee293a3790a2e85e19183a41fc296e09186c8291
Bryan <bryantello2010@hotmail.com>
2021-06-04 22:12:05 -0600

    Agregado el botón de agregar nueva categoría; se listan las categorías en el menú; ya se filtran los resultados

--- gestionDeProductos/database.py
+++ gestionDeProductos/database.py
@@ -24,3 +24,29 @@ class Database:
         CONSTRAINT      uk_categories UNIQUE(category)
         ) ENGINE=InnoDb;
         """)
+
+
+    def getProducts(tv, category):
+        tv.delete(*tv.get_children())
+        cursor.execute(f'SELECT * FROM categories WHERE category = "{category}"')
+        data = cursor.fetchall()
+        if len(data) <= 0:
+            print('No hay datos')
+        else:
+            for i in data:
+                print(f'Categoría: {i}' )
+                tv.insert('', 'end', values=i)
+
+        print('terminó')
+    
+    def getCategories():
+        cursor.execute('SELECT * FROM categories;')
+        data = cursor.fetchall()
+
+        return data
+
+    def addCategory(category):
+        print(category)
+    
+    def addProdcut(category, product, value, quantity):
+        print(category, product, value, quantity)
--- gestionDeProductos/main.py
+++ gestionDeProductos/main.py
@@ -1,5 +1,7 @@
 from tkinter import Tk
 from tkinter import *
+from tkinter.ttk import Treeview
+from types import FrameType
 from Database import Database
 
 # ----------- DB -------------
@@ -26,18 +28,44 @@ frameMenu.pack(anchor=CENTER)
 Label(frameMenu, text='Gesetión de productos').pack(anchor=CENTER)
 
 # Dropdown
-CATEGORIES = (
-    'computers',
-    'phones',
-    'HDD'
-)
+categoriesList = db.getCategories()
+CATEGORIES = []
+for i in categoriesList:
+    CATEGORIES.append(i[1])
+
 variable = StringVar(root)
 variable.set(CATEGORIES[0])
 
 w = OptionMenu(frameMenu, variable, *CATEGORIES)
 w.pack()
 
+buttonsFrame = Frame(frameMenu)
+buttonsFrame.pack()
 
+def addCategoryWindow():
+    print('Ventana de agregar categoría')
+
+buttonAddCategory = Button(buttonsFrame,
+text='Agregar categoría',
+command=addCategoryWindow)
+buttonAddCategory.pack(side=LEFT)
+
+buttonObtion = Button(buttonsFrame, 
+text='Buscar productos', 
+command=lambda:db.getProducts(table, variable.get())
+)
+buttonObtion.pack(side=LEFT)
+
+
+# ----------- TREEVIEW -------------
+frameTable = Frame(root, padx=10, pady=10)
+frameTable.pack()
+
+table = Treeview(frameTable, show="headings", columns=(0,1, 2), height=10)
+table.heading(0, text='ID')
+table.heading(1, text='Product')
+table.heading(2, text='Quantity')
+table.pack()
 # ----------- finishing -------------
 # Main loop tkinter
 root.mainloop()
